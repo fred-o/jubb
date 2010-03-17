5e271a464d0b425c8268bbdd15a8ba8f1cf06637
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2010-03-17 18:13:20 +0100

    JubbConsumer now acts on external target and accepts multiple @Consumes annotations.

--- jubb-core/src/main/java/jubb/client/JubbConsumer.java
+++ jubb-core/src/main/java/jubb/client/JubbConsumer.java
@@ -3,6 +3,8 @@ package jubb.client;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -12,37 +14,18 @@ import org.apache.log4j.Logger;
 import jubb.client.JsonJubbClient;
 import jubb.client.JubbConsumer;
 
-public abstract class JubbConsumer {
+public class JubbConsumer {
 	private static final Logger LOG = Logger.getLogger(JubbConsumer.class);
 	private JsonJubbClient client;
 	private ScheduledExecutorService executorService;
-	private Method consumesMethod;
-	private Class<?> consumesClass;
+	private Object target;
+	private Map<Class<?>, Method> consumesMethods = new HashMap<Class<?>, Method>();
 	private Worker worker;
 
 	public JubbConsumer() {
-		for(Method m: getClass().getMethods()) {
-			Consumes c = m.getAnnotation(Consumes.class);
-			if (c != null) {
-				if (this.consumesMethod != null) {
-					throw new IllegalArgumentException("Only one consumer method allowed, sorry.");
-				} 
-				Class<?>[] args = m.getParameterTypes();
-				if (args.length != 1) {
-					throw new IllegalArgumentException("Method " + m.getName() + " can only accept ono argument.");
-				} 
-				if (args[0].isAssignableFrom(c.value())) {
-					System.out.println("Found consumer method: " + m.getName());
-					this.consumesClass = c.value();
-					this.consumesMethod = m;
-					m.setAccessible(true);
-				} 
-			} 
-		}
-		if (consumesMethod == null) {
-			throw new IllegalArgumentException("No consumer method found ,sorry");
-		} 
 	}
+
+	
 
 	public void start() {
 		if (executorService == null) {
@@ -66,20 +49,48 @@ public abstract class JubbConsumer {
 	    this.executorService = executorService;
 	}
 
-	class Worker implements Runnable {
+	public void setTarget(Object target) {
+		this.consumesMethods.clear();
+		for(Method m: target.getClass().getMethods()) {
+			Consumes c = m.getAnnotation(Consumes.class);
+			if (c != null) {
+				Class<?>[] args = m.getParameterTypes();
+				if (args.length != 1) {
+					throw new IllegalArgumentException("Method " + m.getName() + " must only accept one argument.");
+				} 
+				if (args[0].isAssignableFrom(c.value())) {
+					System.out.println("Found consumer method: " + m.getName());
+					m.setAccessible(true);
+					this.consumesMethods.put(c.value(), m);
+				} 
+			} 
+		}
+		if (consumesMethods.isEmpty()) {
+			throw new IllegalArgumentException("No consumer method found ,sorry");
+		} 
+	    this.target = target;
+	}
 
+	class Worker implements Runnable {
 		public void run() {
 			Object val = client.take();
-			try {
-				consumesMethod.invoke(JubbConsumer.this, val);
-			}
-			catch (InvocationTargetException ite) {
-				LOG.error("Error invoking consumer", ite);
-			}
-			catch (IllegalAccessException iae) {
-				LOG.error("Error invoking consumer", iae);
+			if(val != null) {
+				try {
+					Method m = consumesMethods.get(val.getClass());
+					if (m != null) {
+						m.invoke(target, val);
+					} else {
+						LOG.warn("Cannot handle item with of class " + val.getClass());
+					}
+				
+				}
+				catch (InvocationTargetException ite) {
+					LOG.error("Error invoking consumer", ite);
+				}
+				catch (IllegalAccessException iae) {
+					LOG.error("Error invoking consumer", iae);
+				}
 			}
 		}
 	}
-
 }
